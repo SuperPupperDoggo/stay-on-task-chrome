a30f8499010dda01e8df945514ff9261fee083f4
SuperPupperDoggo <65412993+SuperPupperDoggo@users.noreply.github.com>
2020-10-25 13:26:25 -0400

    Minor code cleanup

--- distractionalert.js
+++ distractionalert.js
@@ -3,60 +3,60 @@
 //  distractionalert.js - Main script of the Stay On Task Chrome extension
 
 //Setup Variables
-var run = true //Whether or not the popup should appear
-var dismissCounter = -1 //How many times the user has dismissed the popup. It is shown and quickly hidden during page loading to start the setTimeout() loop, thus it is set to -1
+var run = true; //Whether or not the popup should appear
+var dismissCounter = -1; //How many times the user has dismissed the popup. It is shown and quickly hidden during page loading to start the setTimeout() loop, thus it is set to -1
 var date = new Date(); //The Current Date. Used for the disabled weekdays option.
-var hostname_full = window.location.hostname //Used for matching against non-hardcoded rules
-var hostname = window.location.hostname.split(".") //splits up the hostname into each of it's pieces. Really only used to get top level domain.
-var tlDomain = hostname[hostname.length-1] //Top Level Domain. Used in special rule cases.
-var siteDisableList = [] //In case setting(s) don't load properly, it will use the default value(s)
-var siteEnableList = ["scratch.mit.edu"]
-var popupDelay = 60000 
-var disabledDays = [0,4,6]
-var navOpen = false //Is the popup open? Used to prevent accedental incrementation of the dismiss counter
+var hostname_full = window.location.hostname; //Used for matching against non-hardcoded rules
+var hostname = window.location.hostname.split("."); //splits up the hostname into each of it's pieces. Really only used to get top level domain.
+var tlDomain = hostname[hostname.length-1]; //Top Level Domain. Used in special rule cases.
+var siteDisableList = []; //In case setting(s) don't load properly, it will use the default value(s)
+var siteEnableList = ["scratch.mit.edu"];
+var popupDelay = 60000; //time between the appearance of the popup in ms
+var disabledDays = [0,4,6]; //weekdays on which the popup should be disabled
+var navOpen = false; //Is the popup open? Used to prevent accedental incrementation of the dismiss counter
 
 
 let overlayelement = document.createElement("div", [id='distractionOverlay']);
-overlayelement.setAttribute("id","distractionOverlay")
-let overlayText1 = document.createElement("a")
+overlayelement.setAttribute("id","distractionOverlay");
+let overlayText1 = document.createElement("a");
 var linkText = document.createTextNode("Are you still on task?");
-      overlayText1.appendChild(linkText);
-      overlayText1.title = "Are you still on task?";
-      overlayText1.href = "javascript:void(0)";
-      overlayText1.className = "overlay-content"
-var dismissText = document.createTextNode("You have dismissed this popup ")
-var dismissTextC = document.createElement("div")
-dismissTextC.appendChild(dismissText)
-dismissTextC.className = "overlay-text"
-var dismissCountText = document.createTextNode(0)
-var dismissCountTextC = document.createElement("div")
-dismissCountTextC.appendChild(dismissCountText)
-dismissCountTextC.className = "overlay-text"
-var dismissText2 = document.createTextNode(" times on this tab")
-var dismissText2C = document.createElement("div")
-dismissText2C.appendChild(dismissText2)
-dismissText2C.className = "overlay-text"
-overlayelement.className = "overlay"
-overlayelement.appendChild(overlayText1)
-overlayelement.appendChild(dismissTextC)
-overlayelement.appendChild(dismissCountTextC)
-overlayelement.appendChild(dismissText2C)
-document.body.append(overlayelement)
+overlayText1.appendChild(linkText);
+overlayText1.title = "Are you still on task?";
+overlayText1.href = "javascript:void(0)";
+overlayText1.className = "overlay-content";
+var dismissText = document.createTextNode("You have dismissed this popup ");
+var dismissTextC = document.createElement("div");
+dismissTextC.appendChild(dismissText);
+dismissTextC.className = "overlay-text";
+var dismissCountText = document.createTextNode(0);
+var dismissCountTextC = document.createElement("div");
+dismissCountTextC.appendChild(dismissCountText);
+dismissCountTextC.className = "overlay-text";
+var dismissText2 = document.createTextNode(" times on this tab");
+var dismissText2C = document.createElement("div");
+dismissText2C.appendChild(dismissText2);
+dismissText2C.className = "overlay-text";
+overlayelement.className = "overlay";
+overlayelement.appendChild(overlayText1);
+overlayelement.appendChild(dismissTextC);
+overlayelement.appendChild(dismissCountTextC);
+overlayelement.appendChild(dismissText2C);
+document.body.append(overlayelement);
 
 
   chrome.storage.sync.get({
     sitedisablelist: "",
     siteenablelist: "scratch.mit.edu",
-    popupdelay: 60000,
+    popupdelay: 60000, //in miliseconds
     disabledWeekDays: "0,6,4"
   }, function(items) {
     //For some reason despite the variables having been defined outside this function, the values are still lost when the function ends. So I just put everything inside it
-    siteDisableList = items.sitedisablelist.split(",")
-    siteEnableList = items.siteenablelist.split(",")
+    siteDisableList = items.sitedisablelist.split(",");
+    siteEnableList = items.siteenablelist.split(",");
     popupDelay = items.popupdelay;
-    disabledWeekDays = items.disabledWeekDays.split(",")
+    disabledWeekDays = items.disabledWeekDays.split(",");
     if (disabledDays.indexOf(date.getDay) !== -1) {
-      run = false
+      run = false;
   }
   //Special case with .edu domains
   if (tlDomain == "edu" && siteEnableList.indexOf(hostname_full) == -1) {
@@ -77,9 +77,7 @@ document.body.append(overlayelement)
     overlayelement.style.height = "0%";
     dismissCounter = dismissCounter + 1
     navOpen = false
-
     dismissCountText.nodeValue = dismissCounter
-    
   }
   //Now that closeNav() has been defined...
   overlayText1.addEventListener('click',function() {
@@ -88,10 +86,9 @@ document.body.append(overlayelement)
     navOpen = false
     }
   })
-//If the site should have the stay on task popup, then start the setTimeout() loop
-  if (run == true) {
-      
+  //If the site should have the stay on task popup, then start the setTimeout() loop
+  if (run == true) { 
      openNav();
-     closeNav()
+     closeNav();
   }
   });
--- options.js
+++ options.js
@@ -7,23 +7,23 @@ var popupdelay = document.getElementById("popUpDelay");
 
 //Create a list of weekday numbers that is much easier to parse at runtime
 function createDisabledWeekdays(mon,tue,wed,thu,fri) {
-disabledDays = "0,6"
+disabledDays = "0,6";
 if (mon == true) {
-disabledDays = disabledDays.concat(",1")
+disabledDays = disabledDays.concat(",1");
 }
 if (tue == true) {
-  disabledDays = disabledDays.concat(",2")
+  disabledDays = disabledDays.concat(",2");
 }
 if (wed == true) {
-  disabledDays = cdisabledDays.concat(",3")
+  disabledDays = cdisabledDays.concat(",3");
 }
 if (thu == true) {
-  disabledDays = disabledDays.concat(",4")
+  disabledDays = disabledDays.concat(",4");
 }
 if (fri == true) {
-  disabledDays = disabledDays.concat(",5")
+  disabledDays = disabledDays.concat(",5");
 }
-return disabledDays
+return disabledDays;
 }
 //Restores options from chrome.storage
   function restore_options() {
@@ -52,12 +52,12 @@ function save_options() {
     var setting_sitedisablelist = document.getElementById('siteDisableList').value;
     var setting_siteenablelist = document.getElementById('siteEnableList').value;
     var setting_popupdelay = document.getElementById('popUpDelay').value;
-    var setting_disablewd1 = document.getElementById('disableWD1').checked
-    var setting_disablewd2 = document.getElementById('disableWD2').checked
-    var setting_disablewd3 = document.getElementById('disableWD3').checked
-    var setting_disablewd4 = document.getElementById('disableWD4').checked
-    var setting_disablewd5 = document.getElementById('disableWD5').checked
-    var setting_disabledWeekDays = createDisabledWeekdays(setting_disablewd1,setting_disablewd2,setting_disablewd3,setting_disablewd4,setting_disablewd5)
+    var setting_disablewd1 = document.getElementById('disableWD1').checked;
+    var setting_disablewd2 = document.getElementById('disableWD2').checked;
+    var setting_disablewd3 = document.getElementById('disableWD3').checked;
+    var setting_disablewd4 = document.getElementById('disableWD4').checked;
+    var setting_disablewd5 = document.getElementById('disableWD5').checked;
+    var setting_disabledWeekDays = createDisabledWeekdays(setting_disablewd1,setting_disablewd2,setting_disablewd3,setting_disablewd4,setting_disablewd5);
     chrome.storage.sync.set({
       sitedisablelist: setting_sitedisablelist,
       popupdelay: setting_popupdelay,
@@ -72,7 +72,7 @@ function save_options() {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
       status.textContent = 'Options saved.';
-      console.info("Options Saved")
+      console.info("Options Saved");
       setTimeout(function() {
         status.textContent = '';
       }, 750);
@@ -81,7 +81,7 @@ function save_options() {
   }
   
   window.addEventListener('load', function load(event){
-    setUpPage()
+    setUpPage();
     
   })
 
